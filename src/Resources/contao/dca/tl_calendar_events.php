30bf81766e3eb45db3ca26a9396e7dfdf14b2aff
Sönke <me@eknoes.de>
2019-09-10 10:49:36 +0200

    Add extension

---
+++ src/Resources/contao/dca/tl_calendar_events.php
@@ -0,0 +1,5 @@
+<?php
+$GLOBALS['TL_DCA']['tl_calendar_events']['fields']['addTime']['exclude'] = false;
+$GLOBALS['TL_DCA']['tl_calendar_events']['fields']['addTime']['default'] = 1;
+$GLOBALS['TL_DCA']['tl_calendar_events']['fields']['addTime']['sql'] = "char(1) NOT NULL default '1'";
+?>
